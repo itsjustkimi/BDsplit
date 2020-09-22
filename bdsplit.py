94bcfae75aac1a3aef10ae69a139f6368c9d414f
kimberly pine <58868025+itsjustkimi@users.noreply.github.com>
2020-09-21 21:19:04 -0700

    made some stuff better
    small tweaks, fixing small typo

--- bdsplit.py
+++ bdsplit.py
@@ -5,13 +5,14 @@ msg = "Script for splitting up and compressing large chaptered multi-episode BD 
 
 parser = argparse.ArgumentParser(description = msg) 
 
-# Adding optional argument 
-parser.add_argument("-c", "--Chapters", help = "Set number of chapters per episode.", required=True, type=int) 
-parser.add_argument("-n", "--TotalEpisodes", help = "Set the number of episodes in thie file.", required=True, type=int)
+# Adding arguments
+parser.add_argument("-c", "--Chapters", help = "Set number of chapters per episode", required=True, type=int) 
+parser.add_argument("-n", "--TotalEpisodes", help = "Set the number of episodes in thie file", required=True, type=int)
 parser.add_argument("-p", "--Preset", help = "Set a handbrake preset", required=True, type=str) 
 parser.add_argument("-P", "--PresetFile", help = "Optionally you can load a handbrake preset JSON file with this", default="", type=str)
 parser.add_argument("-s", "--StartingChapter", help = "Set the first chapter of the first episode in the file (default is \"1\")", default=1, type=int)  
-parser.add_argument("-i", "--Input", help = "Input file.", required=True, type=str)
+parser.add_argument("-i", "--Input", help = "Input file", required=True, type=str)
+parser.add_argument("-o", "--Output", help = "Output filename (no file extension, it will output like this \"[input]-[episode 1, 2, 3, etc].mkv\")", type=str)
 
 # Read arguments from command line  
 args = parser.parse_args()
@@ -20,10 +21,12 @@ args = parser.parse_args()
 iterator = args.Chapters
 max_chapter = args.Chapters * args.TotalEpisodes
 counter = 1
+if args.Output is None:
+    args.Output = args.Input
 
-# Main loop 480pmod.json
-for x in range(args.StartingChapter,max_chapter,args.Chapters):
-    cmd = "HandBrakeCLI --preset-import-file \"{0}\" -Z \"{1}\" -c {2}-{3} -i \"{4}\" -o \"{4} - {5}.mkv\"".format(args.PresetFile, args.Preset, args.StartingChapter, args.Chapters, args.Input, counter)
+# Main loop
+for x in range(args.StartingChapter, max_chapter, args.Chapters):
+    cmd = "HandBrakeCLI --preset-import-file \"{}\" -Z \"{}\" -c {}-{} -i \"{}\" -o \"{} - episode {}.mkv\"".format(args.PresetFile, args.Preset, args.StartingChapter, args.Chapters, args.Input, args.Output, counter)
     print(cmd)
     os.system(cmd)
     args.Chapters += iterator
